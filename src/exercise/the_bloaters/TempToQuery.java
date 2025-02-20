df6f9ca8d2879994b379afeee3f483ffc8f7c352
lieja <lieja@Jsnlie>
2025-02-20 14:54:48 +0700

    change this method to using replace temp with query

--- src/exercise/the_bloaters/TempToQuery.java
+++ src/exercise/the_bloaters/TempToQuery.java
@@ -4,11 +4,12 @@ public class TempToQuery {
 	class Product {
 		private int quantity;
 		private int itemPrice;
+
 		public Product(int quantity, int price) {
 			this.quantity = quantity;
 			this.itemPrice = price;
 		}
-		
+
 		// TODO: change this method to using replace temp with query
 		public int basePrice() {
 			return quantity * itemPrice;
@@ -22,12 +23,14 @@ public class TempToQuery {
 			return basePrice() * discountFactor();
 		}
 	}
+
 	public void Test() {
 		Product p1 = new Product(100, 200);
 		Product p2 = new Product(10, 50);
 		System.out.println("Price 1 = " + p1.getPrice());
 		System.out.println("Price 2 = " + p2.getPrice());
 	}
+
 	public static void main(String[] args) {
 		TempToQuery t = new TempToQuery();
 		t.Test();
