f8f21d34eb91544842ede683a3dcb511993420c9
lieja <lieja@Jsnlie>
2025-02-20 14:46:27 +0700

    extract subclass PartsItem & LaborItem from JobItem

--- src/exercise/the_bloaters/ExtractSubClass.java
+++ src/exercise/the_bloaters/ExtractSubClass.java
@@ -1,41 +1,67 @@
 ﻿package exercise.the_bloaters;
+
 public class ExtractSubClass {
 // todo: extract subclass PartsItem & LaborItem from JobItem
-	class JobItem {
-	  private int quantity;
-	  private int unitPrice;
-	  private Employee employee;
-	  private boolean isLabor;
 
-	  public JobItem(int quantity, int unitPrice, boolean isLabor, Employee employee) {
-		this.quantity = quantity;
-		this.unitPrice = unitPrice;
-		this.isLabor = isLabor;
-		this.employee = employee;
-	  }
-	  public int getTotalPrice() {
-		return quantity * getUnitPrice();
-	  }
-	  public int getQuantity() {
-		return quantity;
-	  }
-	  public int getUnitPrice() {
-		return (isLabor) ? employee.getRate() : unitPrice;
-	  }
-	  public Employee getEmployee() {
-		return employee;
-	  }
+	abstract class JobItem {
+		protected int quantity;
+
+		public JobItem(int quantity) {
+			this.quantity = quantity;
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public int getTotalPrice() {
+			return quantity * getUnitPrice();
+		}
+
+		public abstract int getUnitPrice();
+
+	}
+
+	class PartsItem extends JobItem {
+		private int unitPrice;
+
+		public PartsItem(int quantity, int unitPrice) {
+			super(quantity);
+			this.unitPrice = unitPrice;
+		}
+
+		@Override
+		public int getUnitPrice() {
+			return unitPrice;
+		}
+	}
+
+	class isLabor extends JobItem {
+		private Employee employee;
+
+		public PartsItem(int quantity, Employee employee){
+			super(quantity);
+			this.employee = employee;
+		}
+
+		@Override
+		public int getUnitPrice() {
+			return employee.getRate();
+		}
 	}
 
 	class Employee {
-	  private int rate;
-	  public Employee(int rate) {
-		this.rate = rate;
-	  }
-	  public int getRate() {
-		return rate;
-	  }
+		private int rate;
+
+		public Employee(int rate) {
+			this.rate = rate;
+		}
+
+		public int getRate() {
+			return rate;
+		}
 	}
+
 	public void action() {
 		Employee kent = new Employee(50);
 		JobItem j1 = new JobItem(5, 0, true, kent);
@@ -43,6 +69,7 @@ public class ExtractSubClass {
 		int total = j1.getTotalPrice() + j2.getTotalPrice();
 		System.out.println(total);
 	}
+
 	public static void main(String[] args) {
 		new ExtractSubClass().action();
 	}
